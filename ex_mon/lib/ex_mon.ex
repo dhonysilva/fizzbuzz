0ee98a1c9f955c0c78f6965aa75b119f32d2c887
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-06-05 18:34:38 -0300

    Tratamento para o não chamar as ações após game over

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -20,6 +20,14 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    ExMon.Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(ExMon.Game.info())
+
+  defp handle_status(_other, move) do
     move
     |> Actions.fetch_move()
     |> do_move()
