f48f31aa82de18ad954d61a3d73bf4ca910d884c
Dhony Silva <dhonysilva@Dhonys-MacBook-Air.local>
2023-06-06 13:10:14 -0300

    Implementa o teste na função start_game

--- ex_mon/test/ex_mon_test.exs
+++ ex_mon/test/ex_mon_test.exs
@@ -1,6 +1,7 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureIO
   alias ExMon.Player
 
   describe "create_player/4" do
@@ -9,6 +10,19 @@ defmodule ExMonTest do
       expected_response = %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Rafael"}
 
       assert expected_response == ExMon.create_player("Rafael", :chute, :soco, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("Rafael", :chute, :soco, :cura)
+
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+        assert messages =~ "The game has started!"
     end
   end
 end
